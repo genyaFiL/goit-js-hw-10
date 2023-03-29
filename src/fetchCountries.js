4ecfb38015638da1e8403183c43d4d1662b606e6
genyaFiL <genyafil@gmail.com>
2023-03-30 01:34:10 +0300

    moved function fetchCountries to js file

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,19 +1,94 @@
-export class FetchCountriesAPI {
-  #API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
-  #BASE_URL = 'https://api.unsplash.com';
-}
-const API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
-const BASE_URL = 'https://api.unsplash.com';
-fetchCountries(name);
-function fetchCountries(name) {
-  fetch('https://restcountries.com/v3.1/name/{name}')
+import Notiflix from 'notiflix';
+
+const countryListEL = document.querySelector('.country-list');
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const FIELDS = '?fields=name,capital,population,flags,languages';
+
+export const fetchCountries = searchCountry => {
+  if (!searchCountry) {
+    removeAddedMarkup();
+    return;
+  }
+
+  fetch(`${BASE_URL}${searchCountry}${FIELDS}`)
     .then(response => {
-      // Response handling
+      if (!response.ok) {
+        removeAddedMarkup();
+        Notiflix.Notify.failure(`Oops, there is no country with that name'`);
+        throw new Error(response.status);
+      }
+
+      return response.json();
     })
     .then(data => {
-      // Data handling
+      if (data.length > 10) {
+        removeAddedMarkup();
+        Notiflix.Notify.info(
+          `Too many matches found. Please enter a more specific name.'`
+        );
+        return;
+      } else if (data.length > 2) {
+        removeAddedMarkup();
+        createMarkupFoundCountries(data);
+      } else {
+        removeAddedMarkup();
+        createMarkupFoundCountry(data);
+      }
     })
     .catch(error => {
-      // Error handling
+      console.warn(error);
     });
+};
+
+function createLanguagesList(language) {
+  let languagesValue = '';
+  for (const key in language) {
+    languagesValue += `${language[key]}, `;
+  }
+  return languagesValue.slice(0, -2);
+}
+
+function createMarkupFoundCountries(data) {
+  const createMarkup = data
+    .map(
+      ({ name, flags }) =>
+        `
+      <div class='cards__item'>
+        <div class="cards__wraper">
+          <img class='cards__flag' src=${flags.svg} alt="${flags.alt}" height="20">
+          <h2 class='cards__official'>${name.common}</h2>
+        </div>
+      </div>`
+    )
+    .join('');
+
+  countryListEL.insertAdjacentHTML('afterbegin', createMarkup);
+}
+
+function createMarkupFoundCountry(data) {
+  const createMarkup = data
+    .map(
+      ({ name, capital, population, flags, languages }) =>
+        `
+      <div class='cards__item'>
+        <div class="cards__wraper">
+          <img class='cards__flag' src=${flags.svg} alt="${
+          flags.alt
+        }" height="20">
+          <h2 class='cards__official'>${name.official}</h2>
+        </div>
+        <p class='cards__capital'><span class="cards__bold">Capital: </span>${capital}</p>
+        <p class='cards__population'><span class="cards__bold">Population: </span>${population}</p>
+        <p class='cards__languages'><span class="cards__bold">Languages: </span>${createLanguagesList(
+          languages
+        )}</p>
+      </div>`
+    )
+    .join('');
+
+  countryListEL.insertAdjacentHTML('afterbegin', createMarkup);
+}
+
+function removeAddedMarkup() {
+  countryListEL.innerHTML = '';
 }
